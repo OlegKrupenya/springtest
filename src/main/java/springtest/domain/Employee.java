04d9413df6ee99796135a6222bc391fb54290b17
Oleg Krupenya <oleg.krupenya@akvelon.com>
2014-06-10 23:14:22 +0300

    sql

--- src/main/java/springtest/domain/Employee.java
+++ src/main/java/springtest/domain/Employee.java
@@ -1,5 +1,86 @@
 package springtest.domain;
 
-public class Employee {
+import java.io.Serializable;
 
+public class Employee implements Serializable {
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5273694232587391237L;
+    
+    private Long employeeId;
+    private String firstName;
+    private String lastName;
+    private Short age;
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((age == null) ? 0 : age.hashCode());
+        result = prime * result
+                + ((employeeId == null) ? 0 : employeeId.hashCode());
+        result = prime * result
+                + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result
+                + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (age == null) {
+            if (other.age != null)
+                return false;
+        } else if (!age.equals(other.age))
+            return false;
+        if (employeeId == null) {
+            if (other.employeeId != null)
+                return false;
+        } else if (!employeeId.equals(other.employeeId))
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        return true;
+    }
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Short getAge() {
+        return age;
+    }
+    public void setAge(Short age) {
+        this.age = age;
+    }
+    
 }
---
+++ src/main/resources/employees.sql
@@ -0,0 +1,6 @@
+create table employees (
+    employee_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    age SMALLINT
+);
