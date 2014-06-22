8a5ee588ae48aa6c822409ed4878faf75cf82d72
Oleg Krupenya <oleg.krupenya@akvelon.com>
2014-06-22 19:43:23 +0300

    equals and hashcode

--- src/main/java/springtest/domain/Employee.java
+++ src/main/java/springtest/domain/Employee.java
@@ -41,7 +41,54 @@ public class Employee implements Serializable {
     public Employee() {}
     
     
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((age == null) ? 0 : age.hashCode());
+        result = prime * result
+                + ((employeeId == null) ? 0 : employeeId.hashCode());
+        result = prime * result
+                + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result
+                + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (age == null) {
+            if (other.age != null)
+                return false;
+        } else if (!age.equals(other.age))
+            return false;
+        if (employeeId == null) {
+            if (other.employeeId != null)
+                return false;
+        } else if (!employeeId.equals(other.employeeId))
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        return true;
+    }
+
+
+
     public Long getEmployeeId() {
         return employeeId;
     }
